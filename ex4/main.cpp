51300ef8bce8c12b292bce7578d4f2e5b824f702
1shaked <shaked1hen@gmail.com>
2022-01-02 23:22:23 +0200

    init the project

--- ex4/main.cpp
+++ ex4/main.cpp
@@ -5,10 +5,36 @@
 //  Created by shaked chen on 02/01/2022.
 //
 
+typedef struct {
+    char* firstName;
+    int id;
+    int* grades;
+    float avrage;
+} Student;
+
+typedef struct {
+    char* teacherName;
+    Student* student;
+    int studentsNumber;
+} ClassRoom;
+
 #include <iostream>
+#include <string.h>
+#include <stdio.h>
+// #include <malloc.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    ClassRoom classRome;
+    printf("Hello, please enter teacher name:\n");
+    char teacherName[100];
+    gets(teacherName);
+    // teacherName.length
+    int teacherLen = (int) strlen(teacherName) + 1;
+    printf("%s ,%d \n", teacherName, teacherLen);
+    classRome.teacherName = (char*) malloc(teacherLen * sizeof(char));
+    classRome.teacherName = teacherName;
+    printf("%s ,%d \n", classRome.teacherName, teacherLen);
+    printf("Would you like to add a student (1) yes (else) no?\n");
+    
     return 0;
 }
