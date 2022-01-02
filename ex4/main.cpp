1a3af2655192c8b79d270329208b3299dfcb503f
1shaked <shaked1hen@gmail.com>
2022-01-02 22:55:40 +0200

    Initial Commit

---
+++ ex4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ex4
+//
+//  Created by shaked chen on 02/01/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
