ed276fcece96e44272f04d61e82848baa31d7097
1shaked <shaked1hen@gmail.com>
2022-01-05 14:54:56 +0200

    all the code done

--- ex4/main.cpp
+++ ex4/main.cpp
@@ -24,62 +24,22 @@ typedef struct {
 // #include <malloc.h>
 #define NUMBER_OF_GRADES 4
 #define NUMBER_OF_CHAR 80
+#define NOT_DEFIND_INDEX -1
 int main(int argc, const char * argv[]) {
     ClassRoom classRome;
     printf("Hello, please enter teacher name:\n");
     char* teacherName = (char*) malloc(NUMBER_OF_CHAR * sizeof(char));
     scanf("%s", teacherName);
-    // teacherName.length
     int teacherLen = (int) strlen(teacherName) + 1;
-    classRome.teacherName = (char*) realloc(teacherName, teacherLen * sizeof(char));
-    printf("%s ,%d \n", teacherName, teacherLen); // TODO: DELETE THIS LINE
-    // classRome.teacherName = (char*) malloc(teacherLen * sizeof(char));
-    // classRome.teacherName = teacherName;
     // release the memory
+    classRome.teacherName = (char*) realloc(teacherName, teacherLen * sizeof(char));
     classRome.studentsNumber = 0;
-    printf("%s ,%d \n", classRome.teacherName, teacherLen);
     printf("Would you like to add a student (1) yes (else) no?\n");
     int doYouWantToPlay = 0;
     scanf("%d" , &doYouWantToPlay);
     
-    
-    /*
-     Testing
-     
-    Student s1, s2;
-    // {"s", "H", "K"}
-    s1.username = (char*) malloc( 5 * sizeof(char) );
-    s1.grades = (int*) malloc(3 * sizeof(int));
-    *(s1.grades + 1) = 100;
-    *(s1.grades + 2) = 80;
-    *(s1.grades + 3) = 50;
-    ClassRoom cls;
-    cls.students = (Student*) malloc(3 * sizeof(Student));// malloc(sizeof(Student));
-    cls.students[0] = s1;
-    
-    s2 = s1;
-    cls.students[1] = s2;
-    cls.students[2] = s2;
-    printf("%d \n", s2.grades[0]);
-    cls.students[1].grades[0] = -100;
-    printf("%d \n", s2.grades[0]);
-    // s2 = NULL;
-    s2.grades = (int*) malloc(3 * sizeof(int));
-    *s2.grades = 50;
-    *(s2.grades + 1) = 74;
-    printf("%d %d\n", s2.grades[0], s2.grades[1]);
-    printf("%d %d\n", cls.students[1].grades[0], cls.students[1].grades[1]);
-    
-    cls.students[1].avrage = 1.1;
-    cls.students[2].avrage = 0.9;
-    printf("%f %f\n", cls.students[2].avrage , cls.students[1].avrage);
-    // if (&s2 == &s1) printf("the same addres");
-    
-    
-     End Testing
-     */
-    
-    
+    int lowestAvgIndex = NOT_DEFIND_INDEX;
+    int highestAvgIndex = NOT_DEFIND_INDEX;
     // free(teacherName);
     while (doYouWantToPlay == 1) {
         Student tempStudent;
@@ -94,13 +54,14 @@ int main(int argc, const char * argv[]) {
         scanf("%d", &tempStudent.id);
         // this will reinit the grades location
         tempStudent.grades = (int*) malloc(NUMBER_OF_GRADES * sizeof(int));
+        tempStudent.avrage = 0;
         int gradeIndex = 0;
         for (; gradeIndex < NUMBER_OF_GRADES; gradeIndex++) {
             int grade;
             scanf("%d", &grade);
             tempStudent.grades[gradeIndex] = grade;
             // to reduce the amount of loops needed and calculate each grade effect
-            tempStudent.avrage = grade / NUMBER_OF_GRADES;
+            tempStudent.avrage = tempStudent.avrage + grade / NUMBER_OF_GRADES;
         }
         
         classRome.studentsNumber = classRome.studentsNumber + 1;
@@ -109,17 +70,41 @@ int main(int argc, const char * argv[]) {
         }
         else classRome.students = (Student*) realloc(classRome.students ,classRome.studentsNumber * sizeof(Student));
         int lastIndex = classRome.studentsNumber - 1;
-        char ptr = *(classRome.students + lastIndex)->username;
-        printf("%c\n", ptr);
         classRome.students[lastIndex].username = tempStudent.username;
         classRome.students[lastIndex].grades = tempStudent.grades;
+        float avrage = tempStudent.avrage;
         classRome.students[lastIndex].avrage = tempStudent.avrage;
+        classRome.students[lastIndex].id = tempStudent.id;
+        
+        if (lowestAvgIndex == NOT_DEFIND_INDEX) lowestAvgIndex = lastIndex;
+        else if (classRome.students[lowestAvgIndex].avrage > avrage) lowestAvgIndex = lastIndex;
+        if (highestAvgIndex == NOT_DEFIND_INDEX) highestAvgIndex = lastIndex;
+        else if (classRome.students[highestAvgIndex].avrage < avrage) highestAvgIndex = lastIndex;
         
         
         printf("Would you like to add a student (1) yes (else) no?\n");
         scanf("%d" , &doYouWantToPlay);
-        
         // remove the pointer of the temp studnet to
+    }
+    // if (highestAvgIndex == lowestAvgIndex) {}
+    if (classRome.studentsNumber) {
+        Student bestStudent = classRome.students[highestAvgIndex], workStudent = classRome.students[lowestAvgIndex];
+        printf("Best student is %s, with ID of %d. Average is %f\n", bestStudent.username, bestStudent.id, bestStudent.avrage);
+        printf("Worst student is %s, with ID of %d. Average is %f\n", workStudent.username, workStudent.id, workStudent.avrage);
+        // get class avg
+        int i = 0;
+        float classAvg = 0;
+        for (; i < classRome.studentsNumber; i++) {
+            classAvg = classAvg + classRome.students[i].avrage / classRome.studentsNumber;
+        }
+        printf("Class average is %f\n", classAvg);
+        printf("Teacher is %s\n", classRome.teacherName);
+        printf("Students are:\n");
+        int index = 0;
+        for (; index < classRome.studentsNumber; index++) {
+            printf("%s\n", classRome.students[index].username);
+        }
+        
     }
     return 0;
 }
